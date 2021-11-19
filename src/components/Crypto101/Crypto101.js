79f942fb770c7344b01de3119622499eec7492ee
Hannah Park <hannahp056@gmail.com>
2021-11-19 10:09:01 -0500

    card section styling

--- src/components/Crypto101/Crypto101.js
+++ src/components/Crypto101/Crypto101.js
@@ -1,4 +1,7 @@
 import "./Crypto101.scss";
+import wizard from "../../assets/images/crypto_wizard.svg";
+import blockchain from "../../assets/images/blockchain.svg";
+import security from "../../assets/images/security.svg";
 
 export default function Crypto101() {
   return (
@@ -10,30 +13,39 @@ export default function Crypto101() {
       <section className="cards-section__cards-container">
         <div className="cards-section__card">
           <h3 className="cards-section__card-title">Card Title</h3>
-          <p lassName="cards-section__card-text">
+          <p className="cards-section__card-text">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
-            mollitia, molestiae quas vel sint commodi repudiandae consequuntur
-            voluptatum laborum numquam blanditiis harum quisquam eius sed odit
-            fugiat iusto fuga praesentium optio, eaque rerum!
+            mollitia, molestiae quas vel sint commodi
           </p>
+          <img
+            src={wizard}
+            alt="mysterious bitcoin person in robes"
+            className="cards-section__card-image"
+          />
         </div>
-        <div className="cards-section__card">
+        <div className="cards-section__card cards-section__card--middle">
           <h3 className="cards-section__card-title">Card Title</h3>
-          <p lassName="cards-section__card-text">
+          <p className="cards-section__card-text">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
-            mollitia, molestiae quas vel sint commodi repudiandae consequuntur
-            voluptatum laborum numquam blanditiis harum quisquam eius sed odit
-            fugiat iusto fuga praesentium optio, eaque rerum!
+            mollitia, molestiae quas vel sint commodi
           </p>
+          <img
+            src={blockchain}
+            alt="person interacting with world of blockchain"
+            className="cards-section__card-image"
+          />
         </div>
-        <div className="cards-section__card">
+        <div className="cards-section__card cards-section__card--right">
           <h3 className="cards-section__card-title">Card Title</h3>
-          <p lassName="cards-section__card-text">
+          <p className="cards-section__card-text">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
-            mollitia, molestiae quas vel sint commodi repudiandae consequuntur
-            voluptatum laborum numquam blanditiis harum quisquam eius sed odit
-            fugiat iusto fuga praesentium optio, eaque rerum!
+            mollitia, molestiae quas vel sint commodi
           </p>
+          <img
+            src={security}
+            alt="abstract digital transaction with security symbols"
+            className="cards-section__card-image"
+          />
         </div>
       </section>
     </article>
