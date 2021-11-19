5602840df3c32cc500fdf891b37f4f305abb95ca
Hannah Park <hannahp056@gmail.com>
2021-11-19 13:00:04 -0500

    create second cards section

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,13 +1,17 @@
-import './Home.scss'
-import Stats from '../../components/Stats/Stats'
-import Hero from '../../components/Hero/Hero'
-import Crypto101 from '../../components/Crypto101/Crypto101'
+import "./Home.scss";
+import Stats from "../../components/Stats/Stats";
+import Hero from "../../components/Hero/Hero";
+import Crypto101 from "../../components/Crypto101/Crypto101";
+import LearnMore from "../../components/LearnMore/LearnMore";
+import CryptoBenefits from "../../components/CryptoBenefits/CryptoBenefits";
 
 export default function Home(props) {
-    return (
-        <div>
-            <Hero/>
-            <Crypto101/>
-        </div>
-    )
+  return (
+    <div>
+      <Hero />
+      <Crypto101 />
+      <LearnMore />
+      <CryptoBenefits />
+    </div>
+  );
 }
