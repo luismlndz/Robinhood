227d76d1ae3cc62206fd715437b6f9f7b2610519
Luis Melendez <lmele014@fiu.edu>
2021-11-19 00:47:51 -0500

    solved merge conflicts

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-<<<<<<< HEAD
 import './App.scss';
 import Home from './pages/Home/Home'
-=======
-import "./App.scss";
 import Header from "./components/Header/Header";
->>>>>>> 1f72d91021d94944e70253e2784f0e47a7b43ddf
 
 export default function App() {
   return (
-<<<<<<< HEAD
     <div className="App">
+      <Header />
       <Home />
     </div>
-=======
-    <>
-      <Header />
-      <div className="App"></div>
-    </>
->>>>>>> 1f72d91021d94944e70253e2784f0e47a7b43ddf
   );
 }
